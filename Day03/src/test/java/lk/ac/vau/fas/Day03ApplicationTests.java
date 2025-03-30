a29eb57cae9761afde540cbee5a3f04063b8d310
DilmyPerera <samadiniperera20@gmail.com>
2025-03-30 11:23:07 +0530

    Import springtool packages

---
+++ Day03/src/test/java/lk/ac/vau/fas/Day03ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
